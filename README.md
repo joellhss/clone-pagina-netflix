12c64c17945d139b851d84b8c174fde2612124ee
Joel <joelhenrique78@gmail.com>
2021-04-23 23:52:19 -0300

    netflix inicial 23-04-2021

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# clone-pagina-netflix
-Interface do Netflix
+# Projeto: interface do netflix :clapper:
+Projeto básico aplicando conhecimentos de HTML, CSS, JavaScript e a biblioteca JQuery, para desenvolver uma interface parecida com a do site de streaming: Netflix.
---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+function assistir() {
+    window.location.href = "https://youtu.be/sEOuJ4z5aTc"
+}
+
+function info() {
+    window.location.href = "https://www.themoviedb.org/tv/48891-brooklyn-nine-nine?language=pt-BR"
+}
+
